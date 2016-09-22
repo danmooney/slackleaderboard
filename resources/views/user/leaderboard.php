6e7b4340c62ca17aad7c1464e63ce2861a2dcfa6
danmooney <dan.r.mooney@gmail.com>
2016-09-22 19:09:51 -0400

    Incorporate $total_mutual_reactions_for_this_user_by_user_id_and_reaction_id

--- resources/views/user/leaderboard.php
+++ resources/views/user/leaderboard.php
@@ -10,7 +10,7 @@ use App\Models\User;
 
 $emojis_by_reaction_id = $emojis->generateFlatArrayByKey();
 //$total_reaction_count_among_all_users = $users->getTotalReactionCountAmongAllUsers();
-
+$users_by_user_id = [];
 ?>
 <h3>
 	User:
@@ -33,6 +33,7 @@ $emojis_by_reaction_id = $emojis->generateFlatArrayByKey();
 	<?php
 		foreach ($reactions_to_this_users_posts_grouped_by_user as $reaction_user):
 			$user = $users->find($reaction_user->user_id);
+			$users_by_user_id[$reaction_user->user_id] = $user;
 			if (!$user->isEligibleToBeOnLeaderBoard()) continue;
 			$total_reaction_count_title = sprintf('%s%% of all user\'s reactions', round(($reaction_user->total_reactions_to_this_users_posts / $user->total_reaction_count) * 100, 2));
 			?>
@@ -53,6 +54,70 @@ $emojis_by_reaction_id = $emojis->generateFlatArrayByKey();
 						$emojis_output_for_this_user_count = 0;
 
 						foreach ((array) $reaction_user->reaction_count_by_reaction_id as $reaction_id => $total_count):
+							if ($emojis_output_for_this_user_count === 10) {
+								break;
+							}
+
+							$reaction = $emojis_by_reaction_id[$reaction_id];
+
+							if (!$reaction) {
+								continue;
+							}
+
+							$emojis_output_for_this_user_count += 1;
+							$anchor_title = sprintf('%s &#013;%s%% of all user\'s reactions', htmlspecialchars($reaction->getMainAlias()->alias), round(($total_count / $user->total_reaction_count) * 100, 2));
+						?>
+							<a class="reaction-anchor" title="<?= $anchor_title ?>" href="<?= action('ReactionController@showLeaderboardAction', [$team->domain, $reaction->getMainAlias()->alias]) ?>">
+								<span class="reaction-img" style="background-image:url('<?= $reaction->image ?>')"></span>
+								<span class="reaction-count"><?= htmlspecialchars($total_count) ?></span>
+							</a>
+					<?php
+						endforeach ?>
+				</td>
+			</tr>
+	<?php
+		endforeach ?>
+	</tbody>
+</table>
+
+<h3><strong>Top Mutual Post Reactor Reactions</strong></h3>
+<table>
+	<thead>
+		<tr>
+			<th>Name</th>
+			<th>Total Mutual Reaction Count</th>
+			<th>Percentage of This User's Total Reactions</th>
+			<th>Top Mutual Reactions (to the same posts)</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php
+		foreach ($total_mutual_reactions_for_this_user_by_user_id_and_reaction_id as $user_id => $data):
+			if (!isset($users_by_user_id[$user_id])) {
+				$users_by_user_id[$user_id] = $users->find($user_id);
+			}
+			$user = $users_by_user_id[$user_id];
+			if (!$user->isEligibleToBeOnLeaderBoard()) continue;
+			$total_reaction_count_title = sprintf('%s%% of all user\'s reactions', round(($reaction_user->total_reactions_to_this_users_posts / $user->total_reaction_count) * 100, 2));
+			arsort($data['reactions'], SORT_NUMERIC);
+			?>
+			<tr>
+				<td>
+					<a href="<?= action('UserController@showLeaderboardAction', [$team->domain, $user->handle]) ?>">
+						<img class="user-avatar" width="<?= User::DEFAULT_AVATAR_SIZE ?>" src="<?= htmlspecialchars($user->getAvatar()) ?>" /><?= htmlspecialchars($user->name_binary) ?>
+					</a>
+				</td>
+				<td align="right">
+					<strong><?= htmlspecialchars($data['total']) ?></strong>
+				</td>
+				<td align="right">
+					<?= round(($data['total'] / $user->total_reaction_count) * 100, 2) ?>%
+				</td>
+				<td>
+					<?php
+						$emojis_output_for_this_user_count = 0;
+
+						foreach ((array) $data['reactions'] as $reaction_id => $total_count):
 							if ($emojis_output_for_this_user_count === 10) {
 								break;
 							}
