86fa8b9f7bf45b1434fbcd14cefe42ed16e170b6
danmooney <dan.r.mooney@gmail.com>
2016-09-22 19:03:59 -0400

    Add model Reaction::getReactionByAlias

--- app/Models/Reaction.php
+++ app/Models/Reaction.php
@@ -1,9 +1,24 @@
 <?php
 
 namespace App\Models;
+use DB;
 
 class Reaction extends ModelAbstract
 {
+	protected $guarded = [];
+
+	public static function getReactionByAlias($reaction_alias)
+	{
+		$row = DB::table('reaction AS r')
+			->join('reaction_alias AS ra', 'r.reaction_id', '=', 'ra.reaction_id')
+			->where('ra.alias', '=', $reaction_alias)
+			->select('r.*')
+			->first()
+		;
+
+		return $row ? new static((array) $row) : $row;
+	}
+
 	public function aliases()
 	{
 		return $this->hasMany(ReactionAlias::class);
