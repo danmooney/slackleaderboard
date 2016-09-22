2001537b87079ce9d981fe38586e25ee55a8d0a8
danmooney <dan.r.mooney@gmail.com>
2016-09-21 20:25:35 -0400

    Add layout/main.php

---
+++ resources/views/layout/main.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial=scale=1">
+    <title><?= View::yieldContent('title', 'Slack Leaderboard') ?></title>
+    <link href="/css/app.css" rel="stylesheet">
+    <link href="/css/styles.css" rel="stylesheet">
+    <?= View::yieldContent('style') ?>
+</head>
+<body>
+<header>
+<h1><a href="/">Slack Leaderboard</a></h1>
+	<?php
+		if (isset($__data['team'])): ?>
+			<h2>
+				<a href="<?= action('TeamController@showLeaderboardAction', [$team->domain]) ?>"><img class="user-avatar" width="32" src="<?= $__data['team']->icon ?>" /><?= $__data['team']->name ?></a>
+			</h2>
+	<?php
+		endif ?>
+</header>
+<main>
+	<?= $__data['content'] ?>
+	<script src="/js/jquery.min.js"></script>
+</main>
+</body>
+</html>
