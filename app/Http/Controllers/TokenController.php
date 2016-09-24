5342b1292d375336c85df93e6a0f1cf1086a814c
danmooney <dan.r.mooney@gmail.com>
2016-09-24 13:49:37 -0400

    Add TokenController

---
+++ app/Http/Controllers/TokenController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Token;
+use App;
+use DB;
+use Input;
+use Frlnc\Slack\Http\SlackResponseFactory;
+use Frlnc\Slack\Http\CurlInteractor;
+use Frlnc\Slack\Core\Commander;
+
+class TokenController extends Controller
+{
+	public function getAndStoreTokenFromOauthFlow()
+	{
+		$code 		   = isset($_GET['code']) ? $_GET['code'] : '';
+		$client_id 	   = config('app.slack_client_id');
+		$client_secret = config('app.slack_client_secret');
+		$redirect_uri  = "http://{$_SERVER['HTTP_HOST']}/c";
+
+		$slack_token   = config('app.slack_access_token');
+
+		$interactor    = new CurlInteractor;
+        $interactor->setResponseFactory(new SlackResponseFactory());
+        $commander     = new Commander($slack_token, $interactor);
+
+		$data = [
+			'code' 		    => $code,
+			'client_id'     => $client_id,
+			'client_secret' => $client_secret,
+			'redirect_uri'  => $redirect_uri,
+		];
+
+		$response = $commander->execute('oauth.access', $data);
+
+		$response = $response->getBody();
+
+		$token = new Token();
+		$token->token = isset($response['access_token']) ? $response['access_token'] : '';
+		$token->save();
+
+		setcookie('slack', md5($token->token), strtotime('now + 1 year'));
+
+		return redirect()->action(
+			'TeamController@showLeaderboardAction', ['domain' => 'digitalsurgeons']
+		);
+	}
+}
