23c5b9f0dd670df8e7ffc89497e536d1d28c9cdb
danmooney <dan.r.mooney@gmail.com>
2016-09-27 23:14:33 -0400

    Add Team::importFromSlackResponseBody

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,5 +4,21 @@ namespace App\Models;
 
 class Team extends ModelAbstract
 {
+	public static function importFromSlackResponseBody(array $response_body)
+	{
+		$team_response = $response_body['team'];
+		$slack_team_id = $team_response['id'];
 
+		$team = static::where('slack_team_id', $slack_team_id)->first() ?: new Team();
+
+		$team->slack_team_id = $slack_team_id;
+		$team->name          = $team_response['name'];
+		$team->domain        = $team_response['domain'];
+		$team->email_domain  = $team_response['email_domain'];
+		$team->icon          = $team_response['icon']['image_original'];
+
+		$team->save();
+
+		return $team;
+	}
 }
