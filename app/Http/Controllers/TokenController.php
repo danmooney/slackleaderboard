96a8d281e7d6b7d824cbe840faaa8494c300ff87
danmooney <dan.r.mooney@gmail.com>
2016-10-18 18:40:38 -0400

    Format

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -18,57 +18,57 @@ class TokenController extends Controller
 {
 	public function getAndStoreTokenFromOauthFlow()
 	{
-		$code 		   = isset($_GET['code']) ? $_GET['code'] : '';
-		$client_id 	   = config('app.slack_client_id');
-		$client_secret = config('app.slack_client_secret');
-		$redirect_uri  = "http://{$_SERVER['HTTP_HOST']}/c";
+        $code 		   = isset($_GET['code']) ? $_GET['code'] : '';
+        $client_id 	   = config('app.slack_client_id');
+        $client_secret = config('app.slack_client_secret');
+        $redirect_uri  = "http://{$_SERVER['HTTP_HOST']}/c";
 
-		$my_slack_access_token   = config('app.slack_access_token');
+        $my_slack_access_token   = config('app.slack_access_token');
 
 		$interactor    = new CurlInteractor;
         $interactor->setResponseFactory(new SlackResponseFactory());
         $commander     = new Commander($my_slack_access_token, $interactor);
 
-		$data = [
-			'code' 		    => $code,
-			'client_id'     => $client_id,
-			'client_secret' => $client_secret,
-			'redirect_uri'  => $redirect_uri,
-		];
+        $data = [
+            'code' 		    => $code,
+            'client_id'     => $client_id,
+            'client_secret' => $client_secret,
+            'redirect_uri'  => $redirect_uri,
+        ];
 
-		$response = $commander->execute('oauth.access', $data);
-		$response = $response->getBody();
+        $response = $commander->execute('oauth.access', $data);
+        $response = $response->getBody();
 
-		$their_slack_access_token = isset($response['access_token']) ? $response['access_token'] : '';
+        $their_slack_access_token = isset($response['access_token']) ? $response['access_token'] : '';
 
-		DB::beginTransaction();
+        DB::beginTransaction();
 
-		// add team if it doesn't already exist
-		$response = $commander->execute('team.info');
-		$team     = Team::importFromSlackResponseBody($response->getBody());
+        // add team if it doesn't already exist
+        $response = $commander->execute('team.info');
+        $team     = Team::importFromSlackResponseBody($response->getBody());
 
-		// add users
-		$response = $commander->execute('users.list');
-		$users    = UserCollection::importFromSlackResponseBody($response->getBody());
+        // add users
+        $response = $commander->execute('users.list');
+        $users    = UserCollection::importFromSlackResponseBody($response->getBody());
 
-		// get current user's identity and store in session
-		$response      		   = $commander->execute('auth.test');
-		$current_user_slack_id = $response->getBody()['user_id'];
-		$user 	       		   = $users->where('slack_user_id', $current_user_slack_id)->first() ?: new User();
-		
-		$token = Token::find($user->getKey()) ?: new Token();
-		$token->user_id = $user->getKey();
-		$token->token   = $their_slack_access_token;
-		$token->save();
-		
-		setcookie('slack', md5($token->token), strtotime('now + 1 year'));
-		
-		User::saveIntoSession($user);
+        // get current user's identity and store in session
+        $response              = $commander->execute('auth.test');
+        $current_user_slack_id = $response->getBody()['user_id'];
+        $user 	       		   = $users->where('slack_user_id', $current_user_slack_id)->first() ?: new User();
 
-		DB::commit();
+        $token = Token::find($user->getKey()) ?: new Token();
+        $token->user_id = $user->getKey();
+        $token->token   = $their_slack_access_token;
+        $token->save();
 
-		return redirect()->action(
-			'TeamController@showLeaderboardAction', ['domain' => $team->domain]
-		);
+        setcookie('slack', md5($token->token), strtotime('now + 1 year'));
+
+        User::saveIntoSession($user);
+
+        DB::commit();
+
+        return redirect()->action(
+            'TeamController@showLeaderboardAction', ['domain' => $team->domain]
+        );
 	}
 }
