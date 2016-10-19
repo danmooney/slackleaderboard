b25b0c2d4301e7cbdbeb3456c9a9e46743701cec
Cory Zibell <cory.zibell@digitalsurgeons.com>
2016-10-19 18:30:40 -0400

    Removed vue for frontend

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ process.env.DISABLE_NOTIFIER = true;
 
 const elixir = require('laravel-elixir');
 
-require('laravel-elixir-vue');
 
 /*
  |--------------------------------------------------------------------------
