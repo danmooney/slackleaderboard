e55b9d4ae6e7c51b26aecddb12e20f139bcff938
danmooney <dan.r.mooney@gmail.com>
2016-11-01 23:16:04 -0400

    Update models

--- app/Models/PostMessage.php
+++ app/Models/PostMessage.php
@@ -4,5 +4,6 @@ namespace App\Models;
 
 class PostMessage extends ModelAbstract
 {
-
+    protected $primaryKey = 'post_id';
+    public $incrementing = false;
 }
--- app/Models/ReactionFetchLog.php
+++ app/Models/ReactionFetchLog.php
@@ -4,5 +4,15 @@ namespace App\Models;
 
 class ReactionFetchLog extends ModelAbstract
 {
+    protected $primaryKey = 'team_id';
+    protected $guarded = [];
 
+    public $incrementing = false;
+
+	const STALL_CONSIDERATION_THRESHOLD_SECONDS = 60 * 60 * 24;
+
+	public function hasMostLikelyBeenStalled()
+	{
+        return strtotime($this->updated_at) + self::STALL_CONSIDERATION_THRESHOLD_SECONDS < time();
+	}
 }
