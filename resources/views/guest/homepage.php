1bd3f9adf90a66cc58949201d0bedc78a3e48283
danmooney <dan.r.mooney@gmail.com>
2016-11-01 23:16:16 -0400

    Add headline to guest homepage

--- resources/views/guest/homepage.php
+++ resources/views/guest/homepage.php
@@ -1,1 +1,4 @@
-<a href="<?= $sign_in_url ?>"><img src="https://api.slack.com/img/sign_in_with_slack.png" /></a>
+<div style="text-align:center;margin: 10px 0;">
+    <h1>Sign in to see the Slack Reaction Leaderboard for your team!</h1>
+    <a href="<?= $sign_in_url ?>"><img src="https://api.slack.com/img/sign_in_with_slack.png" /></a>
+</div>
