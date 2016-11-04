0295cfcae2a2a60f54f4bd67a64c40cad8831f2a
danmooney <dan.r.mooney@gmail.com>
2016-11-03 20:31:25 -0400

    Add ReactionFetchLog::lastFetchHasBeenMadeTooRecently

--- app/Models/ReactionFetchLog.php
+++ app/Models/ReactionFetchLog.php
@@ -10,9 +10,15 @@ class ReactionFetchLog extends ModelAbstract
     public $incrementing = false;
 
 	const STALL_CONSIDERATION_THRESHOLD_SECONDS = 60 * 60 * 24;
+	const RECENT_THRESHOLD_SECONDS = 60 * 10; // 10 minutes ago
 
 	public function hasMostLikelyBeenStalled()
 	{
         return strtotime($this->updated_at) + self::STALL_CONSIDERATION_THRESHOLD_SECONDS < time();
 	}
+
+	public function lastFetchHasBeenMadeTooRecently()
+    {
+        return !$this->in_process && strtotime($this->updated_at) + self::RECENT_THRESHOLD_SECONDS > time();
+    }
 }
