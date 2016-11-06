869c2f431c3b696b78ef5aab6e9f18b459bda8f2
danmooney <dan.r.mooney@gmail.com>
2016-11-05 20:21:51 -0400

    Add mutator/accessor to Token

--- app/Models/Token.php
+++ app/Models/Token.php
@@ -5,4 +5,14 @@ namespace App\Models;
 class Token extends ModelAbstract
 {
     protected $primaryKey = 'user_id';
+
+    public function getTokenAttribute($value)
+    {
+        return decrypt($value);
+    }
+
+    public function setTokenAttribute($value)
+    {
+        $this->attributes['token'] = encrypt($value);
+    }
 }
