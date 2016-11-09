29e66637c3b482a178ea1b7598d7d3cb9849347b
danmooney <dan.r.mooney@gmail.com>
2016-11-08 23:41:28 -0500

    Add sort arrow changing

--- resources/assets/js/table-sorter.js
+++ resources/assets/js/table-sorter.js
@@ -3,7 +3,6 @@ require('tablesorter');
 var $tables = $('table');
 
 $tables.each(function () {
-    console.log(this);
     $(this).tablesorter({
         headers: {
             // disable sorting of the first & second column - before we would have to had made two entries
@@ -13,5 +12,19 @@ $tables.each(function () {
                 sorter: false
             }
         }
+    }).on('sortEnd', function () {
+        var $sortedTableHeader = $(this).find('.tablesorter-headerAsc, .tablesorter-headerDesc'),
+            $sortArrow = $sortedTableHeader.find('.fa')
+        ;
+
+        // remove all current existing sort icons
+        $(this).find('th .fa').removeClass('fa-sort-desc').removeClass('fa-sort-asc');
+
+        if (!$sortArrow.length) {
+            $sortArrow = $('<i class="fa fa-fw"></i>');
+            $sortArrow.appendTo($sortedTableHeader.children('.tablesorter-header-inner'));
+        }
+
+        $sortArrow.addClass($sortedTableHeader.hasClass('tablesorter-headerAsc') ? 'fa-sort-asc' : 'fa-sort-desc');
     });
 });
