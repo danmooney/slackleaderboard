08380fbc212d43c1b4ff45fb373dd862a4217f65
danmooney <dan.r.mooney@gmail.com>
2017-01-09 22:28:23 -0500

    Update slack_emoji_fetch.js

--- database/seeds/slack_emoji_fetch.js
+++ database/seeds/slack_emoji_fetch.js
@@ -1,1 +1,17 @@
-var emojis = {}; document.querySelectorAll('[data-name]').forEach(function (emoji) {var headline = $(emoji).closest('.emoji_section_div').children('h3'); if (headline.attr('id') === 'emoji_h3_mine' || headline.attr('id') === 'emoji_h3_slack') {return;} emojis[emoji.getAttribute('data-name')] = emoji.querySelector('.emoji').style.backgroundImage.replace('url("', '').replace('")', '')})
+var emojis = {};
+document.querySelectorAll('[data-name]').forEach(function (emoji) {
+    var headline = $(emoji).closest('.emoji_section_div').children('h3');
+    // emoji_h3_mine is 'frequently used', emoji_h3_slack is 'custom'
+    if (headline.attr('id') === 'emoji_h3_mine' || headline.attr('id') === 'emoji_h3_slack') {
+        return;
+    }
+
+    // if you want aliases too
+    emoji.getAttribute('data-names').split(' ').forEach(function (name) {
+        name = name.trim();
+        emojis[name] = emoji.querySelector('.emoji').style.backgroundImage.replace('url("', '').replace('")', '')
+    });
+
+    // default only
+    // emojis[emoji.getAttribute('data-name')] = emoji.querySelector('.emoji').style.backgroundImage.replace('url("', '').replace('")', '')
+});
