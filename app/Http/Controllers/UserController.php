4a3cb1ce7ba6a12d885e574081ca4e9f4766e8d6
danmooney <dan.r.mooney@gmail.com>
2017-02-18 20:13:31 -0500

    Exclude users ineligible to be on leaderboard and add given/received counts in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use App\Models\Reaction;
 use App\Models\Post;
 use App\Models\PostUserReaction;
+use App\Collections\User as UserCollection;
 use App\Collections\PostUserReaction as PostUserReactionCollection;
 use App\Collections\Reaction as ReactionCollection;
 use App;
@@ -40,14 +41,18 @@ class UserController extends Controller
         $users = User::where(['team_id' => $team->team_id/*, 'handle' => $user_handle*/])->get();
     
         PostUserReactionCollection::getTotalReactionGivenCountsByEachUserOnTeamAndAddToUsers($team, $users);
-    //        PostUserReactionCollection::getAllPostUserReactionsByEachUserOnTeamAndAddToUsers($team, $users);
-    
+
         $user  = $users->where('handle', $user_handle)->first();
-    
-        if (!$user) {
+
+        if (!$user || !$user->isEligibleToBeOnLeaderboard()) {
             App::abort(404);
         }
-    
+
+        // make user collection of just the requested user to satisfy required type hinting
+        $user_collection = new UserCollection([$user]);
+        PostUserReactionCollection::getAllPostUserReactionsByEachUserOnTeamAndAddToUsers($team, $user_collection);
+        $single_user_reaction_received_counts          = PostUserReactionCollection::getCountsOfReactionsReceivedToASingleUsersPostsGroupedByUser($team, $user_collection);
+
         $emojis                                        = ReactionCollection::getReactionsAndReactionAliasesByTeam($team, true);
         $reactions_to_this_users_posts_grouped_by_user = PostUserReactionCollection::getTotalReactionsToASingleUsersPostsGroupedByAllUsers($user, $users);
     
@@ -60,7 +65,8 @@ class UserController extends Controller
             'user'   => $user,
             'emojis' => $emojis,
             'reactions_to_this_users_posts_grouped_by_user' => $reactions_to_this_users_posts_grouped_by_user,
-            'total_mutual_reactions_for_this_user_by_user_id_and_reaction_id' => $total_mutual_reactions_for_this_user_by_user_id_and_reaction_id
+            'total_mutual_reactions_for_this_user_by_user_id_and_reaction_id' => $total_mutual_reactions_for_this_user_by_user_id_and_reaction_id,
+            'single_user_reaction_received_counts' => $single_user_reaction_received_counts
         ]);
     
         return $this->_layout;
