4f68b92444679540a544fa1f5eadd066f28d2033
danmooney <dan.r.mooney@gmail.com>
2017-02-26 09:44:45 -0500

    Determine demo mode in AssignDefaultTeamDomainIfNecessary middleware

--- app/Http/Middleware/AssignDefaultTeamDomainIfNecessary.php
+++ app/Http/Middleware/AssignDefaultTeamDomainIfNecessary.php
@@ -21,15 +21,17 @@ class AssignDefaultTeamDomainIfNecessary
     {
         $team_domain = $request->route(Team::TEAM_DOMAIN_KEY);
 
+        $team_domain_altered_to_be_demo_team = false;
+
         // if no team domain exists, or user is using the demo team facade, set the appropritate demo team on the route/request objects
         if (!$team_domain || $team_domain === TEAM::DEMO_TEAM_DOMAIN_FACADE) {
             $request->merge([Team::TEAM_DOMAIN_KEY => Team::DEMO_TEAM_DOMAIN]);
             $request->route()->setParameter(Team::TEAM_DOMAIN_KEY, Team::DEMO_TEAM_DOMAIN);
-            $team_domain = Team::DEMO_TEAM_DOMAIN;
+            $team_domain_altered_to_be_demo_team = true;
         }
 
         $user = User::getFromSession();
-        $is_demo_mode = $team_domain === Team::DEMO_TEAM_DOMAIN && !$user->isLoggedIn();
+        $is_demo_mode = $team_domain_altered_to_be_demo_team && !$user->isLoggedIn();
         App::setDemoMode($is_demo_mode);
 
         return $next($request);
