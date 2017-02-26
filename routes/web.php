1d4fbd122f628b6dd50d58c1e582d5569bd7b667
danmooney <dan.r.mooney@gmail.com>
2017-02-26 10:47:10 -0500

    Remove SlackController and reassign routes to use TeamController@showLeaderboardAction in its place

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,15 @@
 |
 */
 
-Route::get('/', 'SlackController@guestHomepageAction');
+
 Route::get('/c', 'TokenController@getAndStoreTokenFromOauthFlow');
 Route::get('/+/login', 'UserController@loginAction');
 
+Route::get('/{team_domain?}', 'TeamController@showLeaderboardAction');
+Route::get('/{team_domain}/u/{user_handle}', 'UserController@showLeaderboardAction');
+Route::get('/{team_domain}/r/{reaction_alias}', 'ReactionController@showLeaderboardAction');
+Route::get('/{team_domain}/permalink', 'PermalinkController@fetchAction');
+
 Route::group(['middleware' => 'auth'], function () {
     Route::post('/+/logout', 'UserController@logoutAction');
-    Route::get('/{team_domain}', 'TeamController@showLeaderboardAction');
-    Route::get('/{team_domain}/u/{user_handle}', 'UserController@showLeaderboardAction');
-    Route::get('/{team_domain}/r/{reaction_alias}', 'ReactionController@showLeaderboardAction');
-    Route::get('/{team_domain}/permalink', 'PermalinkController@fetchAction');
 });
