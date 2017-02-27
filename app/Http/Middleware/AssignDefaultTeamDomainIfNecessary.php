5ed50f29de713f62324a3cf6c3c79d72c2cb9f56
danmooney <dan.r.mooney@gmail.com>
2017-02-26 21:51:19 -0500

    Add check for whether single-segment URL contains 'demo-team', and redirect back to homepage if that's the case

--- app/Http/Middleware/AssignDefaultTeamDomainIfNecessary.php
+++ app/Http/Middleware/AssignDefaultTeamDomainIfNecessary.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Http\Controllers\TeamController;
 use App\Models\Team;
 use App\Models\User;
 use App;
@@ -23,8 +24,20 @@ class AssignDefaultTeamDomainIfNecessary
 
         $team_domain_altered_to_be_demo_team = false;
 
+        $team_leaderboard_route_name = TeamController::class . '@showLeaderboardAction';
+        $is_on_team_leaderboard_route = request()->route()->getActionName() === $team_leaderboard_route_name;
+
+        $demo_team_domain_is_only_segment_in_url_and_needs_to_redirect_back_to_homepage = (
+            $team_domain === Team::DEMO_TEAM_DOMAIN_FACADE &&
+            $is_on_team_leaderboard_route
+        );
+
+        if ($demo_team_domain_is_only_segment_in_url_and_needs_to_redirect_back_to_homepage) {
+            return redirect()->to('/');
+        }
+
         // if no team domain exists, or user is using the demo team facade, set the appropritate demo team on the route/request objects
-        if (!$team_domain || $team_domain === TEAM::DEMO_TEAM_DOMAIN_FACADE) {
+        if (!$team_domain || $team_domain === Team::DEMO_TEAM_DOMAIN_FACADE) {
             $request->merge([Team::TEAM_DOMAIN_KEY => Team::DEMO_TEAM_DOMAIN]);
             $request->route()->setParameter(Team::TEAM_DOMAIN_KEY, Team::DEMO_TEAM_DOMAIN);
             $team_domain_altered_to_be_demo_team = true;
